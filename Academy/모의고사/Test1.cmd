b8ea0d72bbe2a85f25b08a2048a671839ca3565b
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 07:51:46 +0900

    Create Test1.cmd

---
+++ Academy/모의고사/Test1.cmd
@@ -0,0 +1,42 @@
+2)	You indicated in the survey that you like to go to the movies. Tell me about the kind of movies
+that you like to go see. Why do you like it? Tell me in detail.
+
+3)	What do you usually do before you go to a movie theater? What do you do after watching the movie?
+  Please tell me about your typical day when you go to the movies.
+  
+4)	When was the last time you watched a movie at a movie theater? What movie did you watch?
+  Who did you go with? Where did you go?
+  
+5)	You indicated that you like jogging. Tell me about your favorite place for jogging.
+  Where is it located? What does it look like? What can you see there? Please give me a full description.
+  
+6)	Tell me about the last time you enjoyed jogging. When was it? Where did you go? Who were you with?
+  Tell me in detail.
+  
+7)	Tell me about an interesting or a memorable experience you had while jogging. What happened?
+  Why was it so memorable to you? Tell me about the experience in detail.
+  
+8)	Which websites do you frequently visit? What services do they offer? 
+  What activities do you do at the site? Also, tell me about the reason why you’re interested in this site.
+
+9)	When did you first surf the internet? How did you become interested in surfing the internet? 
+  Did anyone influence you in surfing the internet? Your interest in surfing the internet may have
+changed over time. How has your interest changed since then?
+  
+10)	Can you describe an interesting or unforgettable experience that you had while surfing the internet?
+  Maybe you found a really good website. Or maybe you met a new friend. Tell me about that experience in as much detail as possible.
+
+11)	You want to buy two tickets for a show during your vacation. Call the box office and ask 3 or 4 questions
+  to find out how you can get tickets for the concert.
+  
+12)	You found out the ticket for the concert you want to go to is sold out. Call your friend, 
+explain her about the situation and provide 2 or 3 alternatives.
+  
+13)	Have you ever had an experience that you couldn’t go to a concert that you really wanted to watch?
+  What was the reason? So, what did you do? Tell me what happened in as much detail as you can.
+  
+14)	Can you describe the area around your home? Are there any parks or schools? Describe the neighborhood
+where you live in as much detail as possible.
+  
+15)	What is your neighborhood like? Are there many new buildings? Do many people move into your neighborhood?
+How has your neighborhood changed over the years?
