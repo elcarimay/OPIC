325202f662e90f88c3d911d8536ba43d30ceadfe
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 07:56:28 +0900

    Create Test3.cmd

---
+++ Academy/모의고사/Test3.cmd
@@ -0,0 +1,42 @@
+2)	Tell me about some household chores you do at home. Do you do the dishes or do the laundry?
+  What else do you do?
+  
+3)	I’d like to know what you did at home in your childhood. What were your responsibilities at home?
+  How often did you do it?
+  
+4)	When was the last time you did any household chores? What steps did you take during your chores?
+  How did your house change?
+  
+5)	You indicated in the survey that you like to go to the beach. Tell me about your favorite beach.
+  What does it look like? What can you see in the beach? Give me a full description.
+  
+6)	When was the last time you visited a beach? Who did you go with? Why did you go there?
+  What did you do there? Tell me everything.
+  
+7)	Tell me about an interesting event that happened at a beach. What was so interesting about it?
+  Give me all the details of the event.
+  
+8)	What kinds of things do you usually do in order to maintain your health? And tell me about the
+foods you eat to become healthy. What are they? Tell me in as much detail as possible.
+
+9)	Let’s talk about a healthy person you know. What does he or she look like? What are their daily
+routines like? What do they do or don’t they do to keep their health?
+  
+10)	Tell me about the changes in how people maintain their health. Compared to the past, what has 
+changed in the way people maintain their health? How is it different? Tell me in as much detail as you can.
+
+11)	Your friend and you want to go to a popular wine bar. Call the bar and ask for reservation,
+  asking 3 or 4 questions.
+  
+12)	There is a problem. You found out the bar is not opened that day. Call your friend, tell him about
+the situation and make some suggestions to find another time.
+  
+13)	Have you ever had an experience that you had to change your plan? What was the reason? How did you feel?
+  Tell me about the experience in detail.
+
+14)	These days, many people have their own cell phone and they can talk to people anytime and anywhere.
+  Do you think there are any advantages of chatting on the phone? If so, tell me about them in detail.
+	Well 00?? Um i think it’s really easy and convenient.. so in my case  + 육하원칙
+  
+15)	I guess the pattern of your phone usage has changed. In the past, who did you talk with?
+  And what did you usually talk about? Is it the same or has it changed? If it has changed, how is it different? 
