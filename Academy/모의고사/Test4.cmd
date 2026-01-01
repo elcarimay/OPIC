d9218354a2e187ea529d4db2ef3d8f1fc84875a1
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 07:57:39 +0900

    Create Test4.cmd

---
+++ Academy/모의고사/Test4.cmd
@@ -0,0 +1,40 @@
+2)	Tell me about holidays in your country. What kinds of holidays are there? Which holiday is the biggest?
+What do people do on that day? Is there any special food for the day?
+
+3)	Just pick one of the holidays and describe it to me in detail. What activities do you usually 
+do during the holiday? Please tell me about all the activities from beginning to the end.
+
+4)	Think back to your childhood. I’d like to know about one of your most memorable holidays. 
+Do you have any special memories? If so, what happened? Please tell me about the experience in detail.
+
+5)	What kind of home improvement project do you usually do? How often, and with whom do you enjoy it?
+
+6)	Tell me about the last time that you enjoyed home improvement project. 
+Tell me the process from beginning to the end.
+
+7)	Do you have an experience that you were in trouble while enjoying the home improvement project?
+What happened?
+
+8)	Could you tell me about the geographical features of your country? Are there many mountains or beaches?
+Please describe about it in as much detail as you can.
+
+9)	Describe the last place you visited related to the geography of your country. Where was it? 
+How did you get there? What did you do there? 
+
+10)	 Have you ever been to a geographically unique area in your country? Where was it? 
+What did the place look like? What did you see there? Please tell me in as much detail as possible.
+
+11)	You want to get a new MP3 player. Your friend is an expert on MP3 players. Ask him or her three or four questions. 
+
+12)	You borrowed your friend’s MP3 player, but you accidentally broke it. Call your friend explain the
+situation and make some suggestions to resolve the problem?
+
+13)	Have you ever had any technical or mechanical issue while using any technology? If so, what happened?
+What did you do to resolve the problem? Tell me about it in detail.
+
+14)	Can you tell me about the public transportation system in your country? 
+What kind of public transportation do you like? Why do you like it?
+
+15)	Has the public transportation system changed since you were a child? How is it different? 
+There has been huge progress in the public transportation system to make our lives more convenient.
+What progress have you been experiencing?
