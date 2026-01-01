c990279b30220553154a18f5a085ff6f78e6b8d7
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 07:55:16 +0900

    Create Test2.cmd

---
+++ Academy/모의고사/Test2.cmd
@@ -0,0 +1,41 @@
+2)	I would like to know about the things that you usually do on weekends or on your free time.
+  Could you describe a typical Sunday in detail for me?
+  
+3)	What are some hot places to visit in your free time? It could be a movie theater or a tourist spot.
+  Where do you like to go? Why do you go there?
+  
+4)	I would like to know what you did last weekend. What activities did you do? Where did you go?
+Who were you with? Tell me everything you did last weekend.
+  
+5)	You indicated in the survey that you like riding. When do you enjoy riding? How often do you enjoy
+riding? Who do you enjoy it with? Tell me in detail.
+  
+6)	 How did you first become interested in riding? Who taught you how to ride? What was your first
+impression of riding? Tell me everything you remember.
+  
+7)	Tell me about a memorable experience you had while enjoying riding. What happened? Why do you still
+  remember that experience?
+  
+8)	You indicated in the survey that you enjoy listening to music. How often do you listen to music?
+  When and where do you usually listen to music? And what kind of music do you usually listen to?
+  
+9)	Tell me about a music you enjoyed recently. What music was it? Why did you enjoy that music? 
+  Tell me about that music in as much details as you can.
+  
+10)	I want to know about some of your favorite musicians or composers. Tell me about your two favorite
+musicians or composers and compare them.
+  
+11)	You want to buy a new cell phone. Call the store and ask 3 or 4 questions to find out about the cell
+phone you want to get.
+  
+12)	You have got a new cell phone recently, but it doesn’t work. Call the service center and explain what
+the problem is. Also, give 2 or 3 options to resolve the problem.
+
+13)	Do you have an experience where you had a problem with your cell phone? If so, what was the problem?
+  How did you handle the situation? Tell me in detail.
+  
+14)	Can you tell me about the typical fashions in your country? What kind of styles do people in your 
+country wear day to day? How about the colors?
+  
+15)	What did people in your country wear in the past? How were the clothes different from those of today?
+  Please tell me how clothes have developed or changed over time.
