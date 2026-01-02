a871c5b8134e549a3dd056302794b454052cea97
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 11:52:26 +0900

    Create 심화3.cmd

---
+++ Academy/모의고사/심화3.cmd
@@ -0,0 +1,43 @@
+2)	You indicated in the survey that you like to spend your vacations at home. What kind of activities
+do you usually do on your home vacation? Why do you enjoy that activity? Who do you like to enjoy it with?
+  Tell me in detail.
+  
+3)	What kind of activities do people like to do on their home vacation? What kind of things do young people
+  like to do? And what do old people like to do? Is there any difference in the kind of activities they enjoy?
+  Tell me in as much details as you can.
+
+4)	Tell me about one of your memorable experiences that happened during your home vacation. What happened?
+  Why was it so memorable to you? Tell me about the experience.
+  
+5)	You indicated in the survey that you like to enjoy swimming. Tell me about your swimming routine. 
+  How often do you enjoy swimming? Who do you normally enjoy it with? Where do you usually go to enjoy swimming?
+
+6)	I would like to know how you first became interested in swimming. Is there anyone who taught you how to swim?
+  What made you want to learn swimming? What did you like about it? Tell me all the details you remember.  
+
+7)	When was the last time you enjoyed swimming at a swimming pool? Which swimming pool did you go to? 
+Did you go there on your own? If so, why did you go there alone?
+  
+8)	Tell me about a bank you usually go to. Why do you choose to go to that bank over any other banks?
+What’s special about it?
+  
+9)	How has the bank industry changed compared to the past? What are the changes? Tell me with as many 
+details as you can. 
+
+10)	Have you ever had an experience where a bank teller made a mistake? If so, what happened? 
+  What did you do to solve the problem? Tell me about it in detail.
+  
+11)	A new restaurant opened in downtown and you want to visit there with your friend. Call you
+  r friend, tell her about the restaurant and ask her to go there with you.
+  
+12)	You told your friend that you will pick her up on the way to the restaurant. Unfortunately, 
+  there is a problem. You won’t be able to pick her up. Tell her about the situation and provide 2 or 3 alternatives.
+  
+13)	Have you ever had an experience that you couldn’t keep a promise with someone? What was the reason? 
+  How did the person take your situation?
+  
+14)	You indicated in the survey that you listen to music. Pick two different kinds of music or composers. 
+  Describe each in as much detail as possible. And then compare the similarities and difference between them.
+
+15)	What new electronic gadgets or equipment are people who like music interested in these days? 
+What are they talking about? What new products excite them and why?
