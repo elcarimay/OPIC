cf5c7573343d3e8d9260ac267400b79807cdf0b9
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 11:55:35 +0900

    Create 심화6.cmd

---
+++ Academy/모의고사/심화6.cmd
@@ -0,0 +1,42 @@
+2)	What are some of the free time activities you enjoy? Why do you enjoy that activity? Who do you enjoy
+it with? Tell me about it with as many details as you can.
+  
+3)	Tell me about a place you like to visit in your free time. Why do you like to visit that place?
+  What do you do there? Give me a detailed description about the place you like to visit.
+  
+4)	Do you think you have more free time or less free time compared to the past? Or is it the same?
+  How is it different? Tell me in detail.
+  
+5)	You indicated in the survey that you like going to concerts. What kind of concert do you like to
+go to? Why do you like it? How often do you go to concerts?
+  
+6)	Tell me about one of your recent experiences at a concert. It could be a festival held in your 
+community or a show of a very famous artist. Describe your day with as many details as you can.
+  
+7)	Tell me about a memorable or unforgettable experience you had at a concert or a festival. What 
+  happened? Why was it so memorable to you? Give me a detailed description about everything you remember.
+
+8)	Many people these days eat healthy food to stay fit. What kinds of healthy foods do you eat? 
+  How often do you eat them? Tell me about them with as many details as possible.
+  
+9)	What did you do to maintain your health when you were a child? How have your health habits 
+changed over the years? What remains the same?
+  
+10)	Can you describe a specific incident where you focused on maintaining your health? This could
+involve eating healthy food, exercising or any other activity. Please describe the incident for me.
+  
+11)	You want to make a reservation at a hair salon. Call the hair salon, tell them about the hair 
+style you want and ask 3 or 4 questions to make reservations.
+  
+12)	I’m sorry but there is a problem. You are not happy with your new hair style. Tell the hairdresser
+about the situation and ask what they can do for you.
+  
+13)	Have you ever had an experience that you were not satisfied with the service at a hair salon? 
+  If so, what happened? Why were you not satisfied with it? How did you handle the situation?
+  
+14)	Tell me about the recycling system of your country. How do you recycle? Where do you recycle?
+  What’s different from other countries? Tell me in detail.
+  
+15)	Are there any problems or issues regarding recycling in your country? Perhaps you have heard about
+it on the news. What is it? Are there any ways to resolve the problem? Tell me about it with as many
+details as possible.
