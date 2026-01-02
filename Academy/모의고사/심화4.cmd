78645e941c8d5c4247e596a064d5320ffd18b779
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 11:53:31 +0900

    Create 심화4.cmd

---
+++ Academy/모의고사/심화4.cmd
@@ -0,0 +1,42 @@
+2)	Tell me about some of the responsibilities that you do at home. What are your responsibilities? 
+  How often do you do them? What steps do you take to get them done?
+  
+3)	Think back to your childhood. I would like to know about your responsibilities at home when you were a child.
+What were your responsibilities? Tell me everything you remember.
+  
+4)	Tell me about an issue or a problem you had while getting your responsibilities done. What was the problem?
+  How did you solve the issue? Describe your experience in detail.
+  
+5)	You indicated in the survey that you like to go to the beach. Tell me about a beach you especially like to go.
+  Tell me how you get there from your place and what activities you enjoy there.
+
+6)	What kind of activities do people enjoy at the beach? Give me a detailed description about the kinds of 
+activities people like to do at the beach.
+  
+7)	When was the last time you went to a beach? What kind of activities did you do there? Who were you with? 
+  Tell me about your last trip with as many details as you can.
+
+8)	Describe to me about the weather of each season in your country. What is the weather like in each season?
+  How do you deal with each season?
+  
+9)	How do you think has the weather changed in each season? Compared to the past what has changed? 
+  What stays the same? Tell me in detail.
+  
+10)	Tell me about a memorable experience you had regarding the weather. Maybe there was heavy rain or snowfall.
+  Tell me about all the things you remember about that day.
+  
+11)	There is a swimming pool you want to go to during your holiday. Call the swimming pool and ask 3 or 4 
+  questions about using their facility.
+  
+12)	You noticed a child playing in a deep pool all by himself without a parent or a guardian. Explain this
+  situation to a staff in the swimming pool and provide 2 or 3 suggestions.
+  
+13)	Tell me about one of your memorable or unforgettable experiences that happened in a swimming pool. 
+  What happened? Why was it so unforgettable to you? Tell me in detail.
+
+14)	How has the style of homes changed over the past 5 or 10 years at where you live? Are the homes being built
+today different from the homes built 10 years ago? What developments or changes have you noticed?
+  
+15)	Sometimes issues of too many new homes or not enough new homes can create a controversy in a community. 
+  Think of a time when the issue regarding housing was in the news in your area. What was the issue and 
+why were people talking about it?
