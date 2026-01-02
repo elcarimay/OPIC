1a2462b578c16781e2a42d3181cdd329b3a04361
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 11:48:41 +0900

    Create 심화1.cmd

---
+++ Academy/모의고사/심화1.cmd
@@ -0,0 +1,43 @@
+2)	You indicated in the survey that you like to go to parks. Pick two of the parks that you know and compare them.
+  What are the differences between the two parks? What are the similarities? Give as many details as possible.
+  
+3)	When was the last time you visited a park? What did you do there? Who did you go with? Was there anything 
+  interesting or funny happened at the park? 
+  
+4)	Tell me about a problem or an issue that you had at a park. Perhaps there might have been a maintenance issue
+or maybe a tree fell down. Tell me about your experience in detail.
+
+5)	I would like to know about some of the appointments you make. It could be an appointment with a doctor or a 
+friend or a family member. What kind of appointments do you often make? Describe to me about it in as much detail
+as you can.
+  
+6)	When was the last time you had an appointment for something. What did you do? Who did you meet? 
+Where did you go? Tell me everything.
+  
+7)	Tell me about a memorable episode that you had during your appointment. What happened? Why was it so memorable
+to you? Tell me about the experience in detail.
+
+8)	You indicated that you like shopping. I would like to know about your typical day when you go shopping.
+What do you do before shopping? What do you do after? 
+
+9)	Tell me about your first shopping experience when you were a child. Who did you go with? What do you remember
+  about that time? Tell me about everything you remember.
+  
+10)	What did you buy on your last shopping trip? Why did you buy it? Did you plan on buying it? Are you satisfied
+with the item? Tell me in detail.
+  
+11)	You are hoping to make a reservation at a popular restaurant for your family dinner. You want to find out if
+  it is available to book a seat and ask about the menu. Call the restaurant and ask 3 or 4 questions to get all
+the information you need.
+  
+12)	You found out the restaurant you want to go to is not available on that day. Call your family, tell them about
+the situation and provide 2 or 3 options to resolve the problem.
+
+13)	Have you ever had an issue making a reservation at a restaurant you wanted to visit? What happened? 
+  What kind of problem was it? How did you solve the problem? Tell me in detail.
+  
+14)	Many homes today are equipped with new appliances or electronic devices that make everyday life more convenient.
+  What are some of the new home appliances or electronic devices?
+  
+15)	Do you think there are any negative aspects of development of modern home appliances? 
+  What do people say about these home appliances or devices?
