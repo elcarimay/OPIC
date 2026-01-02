325b661e7f702e811e80e393463e1658ac49f807
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 11:54:26 +0900

    Create 심화5.cmd

---
+++ Academy/모의고사/심화5.cmd
@@ -0,0 +1,43 @@
+2)	Tell me in detail about the process of making reservation at a dentist.
+  How do you usually make an appointment with the doctor?
+  
+3)	Do you remember the first time you visited a dentist? Who did you go with? How did you feel? 
+  What is your memory about that time? Tell me everything that you remember.
+  
+4)	Tell me about an experience where you had to cancel a dentist appointment. 
+  Why did you had to cancel it? Tell me about it in detail.
+
+5)	You indicated in the survey that you like watching movies. What kind of movies do you like to watch?
+  Why do you like watching them? Describe some of your favorite movies with as many details as you can.
+
+6)	Tell me about a movie that you watched recently. What movie was it? Who was starring? 
+  What did you like most about the movie? Tell me about it in detail.
+  
+7)	I would like to know about one of your memorable experiences that you had while watching a movie in
+a movie theater. You might have seen something interesting or something interesting might 
+have happened to you. Tell me about your experience.
+
+8)	You indicated that you like riding a bike. Can you describe your bike? When did you get it? 
+  Where did you get it from? Tell me in detail.
+  
+9)	Do you remember the first time you enjoyed riding your bike? How did you develop your interest
+in bike riding? Tell me everything you remember from your childhood.
+  
+10)	Do you have any memorable or unforgettable experience that happened while you were bike riding?
+  What happened? Why do you remember that experience?
+  
+11)	You have registered for a new gym. You are hoping to find out about the terms and conditions of 
+using the facility. Ask 3 or 4 questions to get all the information you need.
+
+12)	 You have registered for a health program at the gym. However, you won’t be able to make it. 
+  Call the gym, explain your situation and provide 2 or 3 alternatives for attending the program 
+at another time.
+  
+13)	Have you ever participated in a health program at a gym? If so, what did you do there?
+  What kind of health advice did you get from the program? 
+  
+14)	I want to know about a rising industry in your country. Why is that industry rising? Do 
+  you know any promising company in that industry? What’s special about that company?
+  
+15)	Tell me about a company where young people want to work for. 
+  Why do you think they want to work for that company?
