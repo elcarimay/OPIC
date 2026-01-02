4ec3fbca9098ecf7c9345a23e2fb72742c5e4581
elcarimay <103011254+elcarimay@users.noreply.github.com>
2026-01-02 11:50:16 +0900

    Create 심화2.cmd

---
+++ Academy/모의고사/심화2.cmd
@@ -0,0 +1,40 @@
+2)	You indicated in the survey that you like listening to music. What kind of music do you like to listen to?
+  Do you have a musician or a singer you like? Tell me in detail.
+
+3)	Tell me about your typical routine of going to a concert. What do you do before and after you go to a concert?
+Tell me about the process with as many details as you can.
+
+4)	Can you think of a memorable time you heard a live music? What did you like most about it? 
+  Was there anything special? Describe the experience in detail.
+  
+5)	Let’s talk about where you live. I would like to know about your favorite room. Why do you like that room?
+  What do you usually do there? Tell me about the room with as many details as you can.
+  
+6)	Think back to your childhood. What was the house you lived in as a child look like? How is it different
+from the house you currently live in. I would like to know about it in as much detail as possible.
+
+7)	Did you buy any new furniture at home recently? If so, what was it? Why did you get it? 
+  How did your house change? Tell me in detail.
+  
+8)	Tell me about a hair salon you often go to. How did you get to know about that hair salon? 
+  What’s special about it? Why would you recommend it to others?
+  
+9)	When was the last time you went to a hair salon? What did you do there? Were you satisfied with your new look?
+  Did everything turn out as you expected? Tell me about the day with as many details as possible.
+  
+10)	I would like to know about the changes in your hair style over the years. How has it changed? 
+  What do you like and don’t like about your current hair style?
+
+11)	You are planning to visit your friend’s house in a foreign country. Call your friend, ask him 3 or 4 questions
+  to get information about the location. 
+
+12)	Unfortunately, due to the circumstances you won’t be able to visit your friend’s house. Call your friend,
+  tell him about the situation and give 2 or 3 suggestions to find another time to visit his place.
+  
+13)	Have you ever had an experience where you had to cancel a plan or an appointment? What was the reason?
+  What was the person’s reaction? Tell me about it in detail.
+  
+14)	These days people of all ages from children to old people use the internet. 
+  What kind of things do young people like to do and what kinds of things do old people like to do on the internet?
+  
+15)	There are some issues regarding security when surfing the internet. What is your opinion on this?
